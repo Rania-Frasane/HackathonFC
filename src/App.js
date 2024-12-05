73ec60f9b4fa16631c734c4719f010b605391c1b
Farsane Rania <farsanerania52@gmail.com>
2024-12-05 10:58:07 +0100

    rania's commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo } from 'react';
+import { MaterialReactTable, useMaterialReactTable } from 'material-react-table';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+// Données d'exemple
+const data = [
+  {
+    name: {
+      firstName: 'John',
+      lastName: 'Doe',
+    },
+    address: '261 Erdman Ford',
+    city: 'East Daphne',
+    state: 'Kentucky',
+  },
+  {
+    name: {
+      firstName: 'Jane',
+      lastName: 'Doe',
+    },
+    address: '769 Dominic Grove',
+    city: 'Columbus',
+    state: 'Ohio',
+  },
+  // Ajoutez d'autres données au besoin
+];
+
+const SocietesComponent = () => {
+  // Colonnes à afficher dans la table
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: 'name.firstName',
+        header: 'First Name',
+        size: 150,
+      },
+      {
+        accessorKey: 'name.lastName',
+        header: 'Last Name',
+        size: 150,
+      },
+      {
+        accessorKey: 'address',
+        header: 'Address',
+        size: 200,
+      },
+      {
+        accessorKey: 'city',
+        header: 'City',
+        size: 150,
+      },
+      {
+        accessorKey: 'state',
+        header: 'State',
+        size: 150,
+      },
+    ],
+    [],
   );
-}
 
-export default App;
+  // Utilisation de la hook useMaterialReactTable pour créer la table
+  const table = useMaterialReactTable({
+    columns,
+    data,
+  });
+
+  // Rendu de la table
+  return <MaterialReactTable table={table} />;
+};
+
+export default SocietesComponent;
